a4a63a6d73357453f390a5e3b44ee19dcb6f66e1
no_0 <no_.0@daum.net>
2017-04-19 22:15:14 +0900

    연속된 음성인식 및 서버로 보내기 완성. 상당히 불안정. 홀드상태 되면 오류가 나옴
    (cherry picked from commit 96d71da)

--- app/src/main/java/com/ghostwriter/ghostwriter/StudentActivity.java
+++ app/src/main/java/com/ghostwriter/ghostwriter/StudentActivity.java
@@ -1,27 +1,64 @@
 package com.ghostwriter.ghostwriter;
 
-import android.support.v7.app.AppCompatActivity;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+
 public class StudentActivity extends AppCompatActivity {
 
+
+    String IPadr;
+    String PortN;
+    SocketClient client_Server;
+    ReceiveThread RT;
+
+
+    Socket socket;
+    TextView show;
+    Handler msghandler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student);
 
+        IPadr = "223.194.153.40"; //아이피주소
+        PortN =  "5001"; //포트번호
+        show = (TextView)findViewById(R.id.show);
 
-        TextView textView = (TextView) findViewById(R.id.textView);
-        textView.append("뀨뀨");// 글자 추가+ 추후에 소켓으로 받을때 더 추가
+        msghandler = new Handler() {
+            @Override
+            public void handleMessage(Message hdmsg) {
+                if (hdmsg.what == 1111) {
+                    show.append(hdmsg.obj.toString() + "\n");
+                }
+            }
+        };
+
+        client_Server = new SocketClient(IPadr, PortN);
+
+        client_Server.start();
+
+
 
         //폰트 크기 설정
         Button FontUp = (Button) findViewById(R.id.font_up);
         FontUp.setOnClickListener(new View.OnClickListener() {
-            TextView textView = (TextView) findViewById(R.id.textView);
+            TextView textView = (TextView) findViewById(R.id.show);
             float textSize;
 
             @Override
@@ -36,7 +73,7 @@ public class StudentActivity extends AppCompatActivity {
         Button FontDown = (Button) findViewById(R.id.font_down);
         FontDown.setOnClickListener(new View.OnClickListener() {
             float textSize;
-            TextView textView = (TextView) findViewById(R.id.textView);
+            TextView textView = (TextView) findViewById(R.id.show);
 
             @Override
             public void onClick(View v) {
@@ -47,5 +84,83 @@ public class StudentActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+
+
+
+
+    class SocketClient extends Thread {
+        boolean threadAlive;
+        String ip;
+        String port;
+        String mac;
+
+        //InputStream inputStream = null;
+        OutputStream outputStream = null;
+        BufferedReader br = null;
+
+        private DataOutputStream output = null;
+
+        public SocketClient(String ip, String port) {
+            threadAlive = true;
+            this.ip = ip;
+            this.port = port;
+        }
+
+        @Override
+        public void run() {
+            try {
+                socket = new Socket(ip, Integer.parseInt(port));
+
+                //inputStream = socket.getInputStream();
+                output = new DataOutputStream(socket.getOutputStream());
+                RT = new ReceiveThread(socket);
+                RT.start();
+
+                WifiManager mng = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
+                WifiInfo info = mng.getConnectionInfo();
+                mac = info.getMacAddress();
+
+
+                output.writeUTF(mac);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    class ReceiveThread extends Thread {
+        private Socket socket = null;
+        DataInputStream input;
+
+        public ReceiveThread(Socket socket) {
+            this.socket = socket;
+            try {
+                input = new DataInputStream(socket.getInputStream());
+            } catch (Exception e) {
+            }
+        }
+
+        public void run() {
+            try {
+                while (input != null) {
+                    String msg = input.readUTF();
+                    if (msg != null) {
+                        Log.d(ACTIVITY_SERVICE, "test");
+
+                        Message hdmsg = msghandler.obtainMessage();
+                        hdmsg.what = 1111;
+                        hdmsg.obj = msg;
+                        msghandler.sendMessage(hdmsg);
+                        Log.d(ACTIVITY_SERVICE, hdmsg.obj.toString());
+
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 }
